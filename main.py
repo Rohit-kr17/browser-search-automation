da2a43991f810b50f9968015db193eb72c7721bc
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-03-10 21:23:52 +0530

    added multiple account login

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Description: This script will search for a list of queries on Bing.com and will log you in automatically using your Microsoft account.
-
 import os
 import time
 from dotenv import load_dotenv
@@ -7,15 +6,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-# login variables
+
 load_dotenv()  # load environment variables from .env file
-email = os.getenv("EMAIL")  # Your email address from .env file
-passWord = os.getenv("PASSWORD") # Your password from .env file
+email0 = os.getenv("EMAIL0")  # Your email address from .env file
+passWord0 = os.getenv("PASSWORD0") # Your password from .env file
+email1 = os.getenv("EMAIL1") # Your email address from .env file
+passWord1 = os.getenv("PASSWORD1") # Your password from .env file
+email2 = os.getenv("EMAIL2") # Your email address from .env file
+passWord2 = os.getenv("PASSWORD2") # Your password from .env file
 url = "https://www.bing.com/" # Search engine URL
 
+# creating a list of email addresses from os.getenv
+emails = [email1, email0, email2]
+# creating a list of passwords from os.getenv
+passwords = [passWord1, passWord0, passWord2]
+testQueries = ["Hi", "world quant"]  # this is a test query
 
 # Navigate to the login page
-def login(driver_):
+def login(driver_,email_,passWord_):
+    email = email_
+    passWord = passWord_
     driver = driver_
     driver.get("https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&id=264960&wreply=https%3a%2f%2fwww.bing.com%2fsecure%2fPassport.aspx%3fedge_suppress_profile_switch%3d1%26requrl%3dhttps%253a%252f%252fwww.bing.com%252f%253fwlexpsignin%253d1%26sig%3d16C20CCD6A2B602D072F1E026BD9618B&wp=MBI_SSL&lc=1033&CSRFToken=0843d6ef-a4de-43ce-8ec6-189f121347b6&aadredir=1")
     time.sleep(2)
@@ -33,8 +43,7 @@ def login(driver_):
     time.sleep(2)
     next_button = driver.find_element(By.ID, "idSIButton9")
     next_button.click()
-    time.sleep(10)
-
+    time.sleep(5)
 
 def search(driver_, queries_):
     driver = driver_
@@ -47,18 +56,19 @@ def search(driver_, queries_):
         search_box.submit()
         time.sleep(1)
 
-
-
-
 if __name__ == "__main__":
     # Initialize a new instance of the Chrome driver
-    driver = webdriver.Chrome()
     # List of search queries
     queries = ["Artificial intelligence", "Blockchain technology", "Cloud computing", "Data analytics", "Cybersecurity", "Internet of Things (IoT)", "Virtual reality", "Augmented reality", "Machine learning", "Quantum computing", "3D printing", "Robotics", "Biotechnology", "Neural networks", "Natural language processing",
         "Big data", "Cryptocurrency", "Edge computing", "Fintech",    "Smart homes",    "Smart cities",    "Wireless technology",    "Autonomous vehicles",    "Drones",    "Gaming",    "Mobile devices",    "Social media",    "E-commerce",    "Digital transformation",    "Wearable technology"]
-    login(driver)
-    search(driver, queries)
-    time.sleep(10)
+    
+    for i,j in zip(emails, passwords):
+        email = i
+        passWord = j
+        driver = webdriver.Chrome()
+        login(driver, email, passWord)
+        search(driver, testQueries)
+        time.sleep(5)
     driver.quit()
 
 
--- mobile_emulation.py
+++ mobile_emulation.py
@@ -2,15 +2,19 @@
 import time
 from selenium import webdriver
 import main
+from main import emails, passwords, testQueries
 
-mobile_emulation = {"deviceName": "iPhone 5"}
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-driver = webdriver.Chrome(options=chrome_options)
+
 queries = ["internet", "computer", "science", "technological", "artificial intelligence", "machine learning", "cloud computing", "cybersecurity", "blockchain", "big data", "augmented reality", "virtual reality", "5G", "smart glasses","computer vision","mixed reality","spatial computing","AR cloud","biotechnology","engineering"]
 
-
-main.login(driver)
-main.search(driver, queries)
-time.sleep(10)
-driver.quit()
+for i,j in zip(emails, passwords):
+        email = i
+        passWord = j
+        mobile_emulation = {"deviceName": "iPhone 5"}
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+        driver = webdriver.Chrome(options=chrome_options)
+        main.login(driver, email, passWord)
+        main.search(driver, queries)
+        time.sleep(5)
+        driver.quit()
